4760ece9f02a74d3ca6b9d384c38d0abc12df141
dhananjay <dhananjaydeswal11@gmail.com>
2021-03-30 11:37:32 +0530

    Binary Arithmetic expressions and conditional statement code generator functions added

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@ void filereader(char* ipFile, char* opFile) {
 #endif
 }
 
-string tokens[] = {"make", "assign"};
-const int tokens_size = 2;
+// Just for testing Layer 3
+pair<string, int> tokens[] = {{"make", 0}, {"assign", 1}, {"arith", 2}, {"condition", 3}, {"comeout", 4}};
+const int tokens_size = 5;
 
 struct Command
 {
@@ -19,6 +20,30 @@ struct Command
     string dataType;
     string variableName;
     string value;
+
+    string operation;
+    string operand1;
+    string operand2;
+    string result;
+
+    string conditionalKeyword;
+    string expression;
+
+    void clearCommand()
+    {
+        codeNo = -1;
+        dataType = "";
+        variableName = "";
+        value = "";
+
+        operation = "";
+        operand1 = "";
+        operand2 = "";
+        result = "";
+
+        conditionalKeyword = "";
+        expression = "";
+    }
 };
 
 class AlgoInterpreter
@@ -33,14 +58,20 @@ class AlgoInterpreter
 
     Command command;
 
+    int nestedBlocksCounter;
+
     map<string, int> commandCodeMap;
     map<string, string> declaredVariables; // key=>variable name, value=>data type
 
 
     void analyzer(string& s);
 
+    bool isInteger(string& s);
     void declareVariable();
     void assignValue();
+    void performArithmeticOperation();
+    void createConditionalBlock();
+    void comeOutOfBlock();
 
     void cppCodeGenerator();
 
@@ -49,10 +80,11 @@ public:
     {
         for(int i=0; i<tokens_size; ++i)
         {
-            commandCodeMap[tokens[i]] = i;
+            commandCodeMap[tokens[i].first] = tokens[i].second;
         }
 
         command.codeNo = -1;
+        nestedBlocksCounter = 0;
     }
 
     void interpret(string& s);
@@ -67,10 +99,18 @@ void AlgoInterpreter::interpret(string& s)
 
 void AlgoInterpreter::showCppCode()
 {
-    string op = cppCodeHeader + cppCodeBody + cppCodeFooter;
+    string inBlock = "";
+
+    for(int i=0; i<nestedBlocksCounter; ++i)
+    {
+        inBlock += "\n}";
+    }
+
+    string op = cppCodeHeader + cppCodeBody + inBlock + cppCodeFooter;
     cout<<op<<endl;
 }
 
+// This function is just to test whether the layer 3 is working properly or not when the tokenized command is received from layer 2, that is NLP layer
 void AlgoInterpreter::analyzer(string& ipStr)
 {
     int tokenNo = 0;
@@ -89,6 +129,12 @@ void AlgoInterpreter::analyzer(string& ipStr)
 
         if(tokenNo==0)
         {
+            if(!commandCodeMap.count(token))
+            {
+                cout<<"Error : Initial Command identifying token not found"<<endl;
+                return;
+            }
+
             command.codeNo = commandCodeMap[token];
             continue;
         }
@@ -115,15 +161,62 @@ void AlgoInterpreter::analyzer(string& ipStr)
                     break;
             }
         }
+        else if(command.codeNo==2)
+        {
+            switch(tokenNo)
+            {
+                case 1: command.operation = token;
+                    break;
+                case 2: command.operand1 = token;
+                    break;
+                case 3: command.operand2 = token;
+                    break;
+                case 4: command.result = token;
+                    break;
+            }
+        }
+        else if(command.codeNo==3)
+        {
+            switch(tokenNo)
+            {
+                case 1: command.conditionalKeyword = token;
+                    break;
+                default: command.expression += " "+token;
+            }
+        }
+        else if(command.codeNo==4)
+        {
+            // nothing required to be done here
+        }
     }
 
+}
+
+bool AlgoInterpreter::isInteger(string& s)
+{
+    int n = s.size();
+    for(int i=0; i<n; ++i)
+    {
+        if(s[i]<'0'||s[i]>'9')
+        {
+            if(i==0 && s[i]=='-')
+            {
+                continue;
+            }
+
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void AlgoInterpreter::declareVariable()
 {
     if(declaredVariables.count(command.variableName))
     {
-        cout<<"Error : varibale already declared"<<endl;
+        cout<<"Error : "<<command.variableName<<" varibale already declared"<<endl;
+
         //TODO: Handle error
 
         return;
@@ -152,7 +245,8 @@ void AlgoInterpreter::assignValue()
 {
     if(!declaredVariables.count(command.variableName))
     {
-        cout<<"Error : variable not declared yet"<<endl;
+        cout<<"Error : "<<command.variableName<<" variable not declared yet"<<endl;
+
         //TODO: Handle error
 
         return;
@@ -171,15 +265,98 @@ void AlgoInterpreter::assignValue()
 
 }
 
+void AlgoInterpreter::performArithmeticOperation()
+{
+    string var[3] = {command.operand1, command.operand2, command.result};
+
+    bool flag = 0;
+
+    for(int i=0; i<3; ++i)
+    {
+        if(!declaredVariables.count(var[i]))
+        {
+            //TODO : Handle error
+
+            if(!isInteger(var[i]))
+            {
+                cout<<"Error : "<<var[i]<<" variable not declared yet"<<endl;
+                flag = 1;
+            }
+        }
+    }
+
+    if(flag)
+    {
+        return;
+    }
+
+    string& s = cppCodeBody;
+
+    s += "\n"+command.result+" = "+command.operand1+command.operation+command.operand2+";";
+}
+
+void AlgoInterpreter::createConditionalBlock()
+{
+    string& s = cppCodeBody;
+
+    if(command.conditionalKeyword!="else")
+    {
+        bool isEmpty = true;
+        int n = command.expression.size();
+        for(int i=0; i<n; ++i)
+        {
+            if(s[i]!=' ' && s[i]!='\n' && s[i]!='\t')
+            {
+                isEmpty = false;
+                break;
+            }
+        }
+
+        if(isEmpty)
+        {
+            cout<<"Error : Conditional block cannot be empty"<<endl;
+            return;
+        }
+
+        command.expression = "("+command.expression+")";
+    }
+
+    s += "\n"+command.conditionalKeyword+command.expression+"{\n";
+
+    ++nestedBlocksCounter;
+
+}
+
+void AlgoInterpreter::comeOutOfBlock()
+{
+    if(nestedBlocksCounter==0)
+    {
+        return;
+    }
+
+    cppCodeBody += "\n}";
+
+    --nestedBlocksCounter;
+}
+
 void AlgoInterpreter::cppCodeGenerator()
 {
     switch(command.codeNo)
     {
-    case 0: declareVariable();
-        break;
-    case 1: assignValue();
+        case 0: declareVariable();
+            break;
+        case 1: assignValue();
+            break;
+        case 2: performArithmeticOperation();
+            break;
+        case 3: createConditionalBlock();
+            break;
+        case 4: comeOutOfBlock();
+            break;
+        default: cout<<"Error : command codeNo : "<<command.codeNo<<" not available"<<endl;
     }
 
+    command.clearCommand();
 }
 
 
